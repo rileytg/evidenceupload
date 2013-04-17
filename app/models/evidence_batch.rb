69bccd7929a8a1d01a87fab4f1e7db6ed6f3c79b
Riley Guerin <guerin.riley@gmail.com>
2013-04-17 01:51:50 -0400

    allow nested Evidences on EvidenceBatch

--- app/models/evidence_batch.rb
+++ app/models/evidence_batch.rb
@@ -1,3 +1,6 @@
 class EvidenceBatch < ActiveRecord::Base
-  attr_accessible :email, :name, :phone
+  attr_accessible :email, :name, :phone, :evidences, :evidences_attributes
+  has_many :evidences
+  accepts_nested_attributes_for :evidences, allow_destroy: true, reject_if: proc { |attributes| attributes['url'].blank? }
+
 end
