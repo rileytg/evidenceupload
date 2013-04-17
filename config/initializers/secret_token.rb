28e873fcb6470602e3d22a9d4c8c24ce3bbb9567
Riley Guerin <guerin.riley@gmail.com>
2013-04-17 01:15:27 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evidenceupload::Application.config.secret_token = 'e88449c1e20379107997c59534b0787cd476a2210dcd0880ab054ffa179c2adf3449c1fed4e6c5a032d928e98b28e32db3e07b2503b1fc5155c7fbee09d1de61'
