76c82c22987c51fd9d9fbdcb7d015e4ed3993cb8
Riley Guerin <guerin.riley@gmail.com>
2013-04-17 01:41:56 -0400

    removes all but new/create controller actions and corresponding views

--- app/controllers/evidence_batches_controller.rb
+++ app/controllers/evidence_batches_controller.rb
@@ -1,25 +1,5 @@
 class EvidenceBatchesController < ApplicationController
-  # GET /evidence_batches
-  # GET /evidence_batches.json
-  def index
-    @evidence_batches = EvidenceBatch.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @evidence_batches }
-    end
-  end
-
-  # GET /evidence_batches/1
-  # GET /evidence_batches/1.json
-  def show
-    @evidence_batch = EvidenceBatch.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @evidence_batch }
-    end
-  end
 
   # GET /evidence_batches/new
   # GET /evidence_batches/new.json
@@ -32,10 +12,6 @@ class EvidenceBatchesController < ApplicationController
     end
   end
 
-  # GET /evidence_batches/1/edit
-  def edit
-    @evidence_batch = EvidenceBatch.find(params[:id])
-  end
 
   # POST /evidence_batches
   # POST /evidence_batches.json
@@ -53,31 +29,4 @@ class EvidenceBatchesController < ApplicationController
     end
   end
 
-  # PUT /evidence_batches/1
-  # PUT /evidence_batches/1.json
-  def update
-    @evidence_batch = EvidenceBatch.find(params[:id])
-
-    respond_to do |format|
-      if @evidence_batch.update_attributes(params[:evidence_batch])
-        format.html { redirect_to @evidence_batch, notice: 'Evidence batch was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @evidence_batch.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /evidence_batches/1
-  # DELETE /evidence_batches/1.json
-  def destroy
-    @evidence_batch = EvidenceBatch.find(params[:id])
-    @evidence_batch.destroy
-
-    respond_to do |format|
-      format.html { redirect_to evidence_batches_url }
-      format.json { head :no_content }
-    end
-  end
 end
